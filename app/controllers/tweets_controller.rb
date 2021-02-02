cc4dbb9226ada2a9bafdbee9d3c1c372a560e83b
wandji20 <wbertrand10@gmail.com>
2021-02-02 18:10:09 +0100

    Add columns to users relation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tweets or /tweets.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_090104) do
+ActiveRecord::Schema.define(version: 2021_02_02_165246) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2021_02_02_090104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
+    t.index "\"useranme\"", name: "index_users_on_useranme", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
