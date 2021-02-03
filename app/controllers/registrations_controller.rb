7b9d78a70e0f3960e1aff90e2c9ae1d876481a3e
wandji20 <wbertrand10@gmail.com>
2021-02-03 07:48:00 +0100

    update routes and correct typos in registrains controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController <  Devise::RegistrationsController
 
   private
   def sign_up_params
-    params.require(:users).permit(:name,:username. :email, :password, :password_confirm)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirm)
   end
 
   def account_update_params
-    params.require(:users).permit(:name,:username, :email, :password, :password_confirm, current_password )
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirm, current_password )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
+  devise_for :users, :controllers => {registrations: 'registrations'}
   resources :tweets
   root 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
